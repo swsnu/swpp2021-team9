8f5bf9555077edec50d1a9df0c421199f92e7ef8
zz-zye <htht2808@gmail.com>
2021-11-02 20:14:51 +0900

    added wrapper

--- frontend/src/app/wrapper/index.test.tsx
+++ frontend/src/app/wrapper/index.test.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 import { screen } from '@testing-library/dom';
+import { configureAppStore } from 'store/configureStore';
 import Wrapper from '.';
 
+const store = configureAppStore();
+
+function setup() {
+  const path = '/searchresult';
+  const page = (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path={path} render={() => <Wrapper />} />
+          <Redirect to={path} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
+  return { page };
+}
+
 test('should render', () => {
-  render(<Wrapper />);
+  const { page } = setup();
+  render(page);
   expect(screen.getByTestId('Wrapper')).toBeTruthy();
 });
--- frontend/src/app/wrapper/index.tsx
+++ frontend/src/app/wrapper/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import Header from './Header';
+import PlayerBar from './PlayerBar';
+import { useDispatch } from 'react-redux';
 
-interface Props {}
+interface Props {
+  children?: React.ReactChild | React.ReactChild[];
+}
 
 export default function Wrapper(props: Props) {
-  return <div data-testid="Wrapper">wrapper</div>;
+  return (
+    <div data-testid="Wrapper">
+      wrapper
+      <Header />
+      {props.children}
+      <PlayerBar />
+    </div>
+  );
 }
