336e43b4b1f85283ea54b06ea85f219d772f345b
ccdc-git <titieiti.com@gmail.com>
2021-11-08 01:11:20 +0900

    missing

--- frontend/src/app/wrapper/Header/index.test.tsx
+++ frontend/src/app/wrapper/Header/index.test.tsx
@@ -4,6 +4,21 @@ import { screen } from '@testing-library/dom';
 import Header from '.';
 
 test('should render', () => {
-  render(<Header />);
+  render(
+    <Header
+      onSignInClicked={function (): void {
+        throw new Error('Function not implemented.');
+      }}
+      onSignUpClicked={function (): void {
+        throw new Error('Function not implemented.');
+      }}
+      onSignOutClicked={function (): void {
+        throw new Error('Function not implemented.');
+      }}
+      onProfileClicked={function (): void {
+        throw new Error('Function not implemented.');
+      }}
+    />,
+  );
   expect(screen.getByTestId('Header')).toBeTruthy();
 });
