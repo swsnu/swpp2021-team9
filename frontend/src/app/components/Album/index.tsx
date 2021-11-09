e1be76b5cec3b355d6a56d4cf509398bcba8da91
uzrjny <jenny980426@snu.ac.kr>
2021-11-09 11:49:22 +0900

    Album and Main Page

--- frontend/src/app/components/Album/index.tsx
+++ frontend/src/app/components/Album/index.tsx
@@ -1,7 +1,66 @@
 import * as React from 'react';
+import { Component, useState } from 'react';
+import Image from './AlbumImage.png';
+import '../Album/Album.css';
+import { useHistory } from 'react-router-dom';
+import { Song } from 'utils/urls';
 
-interface Props {}
+interface Props {
+  title: string;
+  singer: string;
+  //thumbnail:string|undefined,
+  //play:string|undefined,
+  //onTitleClicked: Function,
+  //onPlayClicked: Function
+}
+
+//const onTitleClicked = ()=>{//Redirect to SongPage of the clicked song}
+//const onPlayClicked = ()=>{//Resume or pause playing the song}
 
 export default function Album(props: Props) {
-  return <div data-testid="Album">Album</div>;
+  const history = useHistory();
+
+  const [Form, setForm] = useState({
+    title: '',
+    singer: '',
+  });
+
+  const onSubmitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(Form);
+    history.push(Song(0));
+  };
+
+  const styles = {
+    label: 'block text-sm font-medium text-gray-700',
+    input:
+      'mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md',
+  };
+
+  return (
+    <div data-testid="Album" className="flex flex-col items-center">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">
+        {props.title} - {props.singer}
+      </h3>
+      <div className="px-4 py-3 bg-gray-50 text-left sm:px-6">
+        <img
+          className="AlbumImage"
+          src={Image}
+          width="100"
+          height="120"
+          alt="AlbumImage"
+        />
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <div className="px-4 py-3 bg-gray-50 text-left sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Play
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- frontend/src/app/containers/MainPage/index.tsx
+++ frontend/src/app/containers/MainPage/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useMainSlice } from './slice';
 import { useDispatch } from 'react-redux';
+import Album from '../../components/Album/index';
 
 export type Props = {};
 export default function MainPage(props: Props) {
@@ -9,5 +10,13 @@ export default function MainPage(props: Props) {
   const dispatch = useDispatch();
   const { actions } = useMainSlice();
 
-  return <div data-testid="MainPage">Main</div>;
+  //return <div data-testid="MainPage">Main</div>;
+  return (
+    <div data-testid="MainPage" className="flex flex-col items-center">
+      <Album title="Album1" singer="Singer1" />
+      <Album title="Album2" singer="Singer2" />
+      <Album title="Album3" singer="Singer3" />
+      <Album title="Album4" singer="Singer4" />
+    </div>
+  );
 }
