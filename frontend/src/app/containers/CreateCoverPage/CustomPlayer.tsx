fc5a75daee44680e1e17a9f3fcde84a31d7f4118
GoGiants1 <chooi221@snu.ac.kr>
2021-11-09 14:17:10 +0900

    Done create cover step1

--- frontend/src/app/containers/CreateCoverPage/CustomPlayer.tsx
+++ frontend/src/app/containers/CreateCoverPage/CustomPlayer.tsx
@@ -4,162 +4,151 @@ interface Props {}
 
 export default function CustomPlayer({}: Props) {
   const player = useRef<any>(null);
-  const [url, setUrl] = useState<String | null>(null);
-  const [pip, setPip] = useState(false);
-  const [playing, setPlaying] = useState(true);
-  const [controls, setControls] = useState(false);
-  const [light, setLight] = useState(false);
-  const [volume, setVolume] = useState(0.8);
-  const [muted, setMuted] = useState(false);
-  const [played, setPlayed] = useState(0);
-  const [loaded, setLoaded] = useState(0);
-  const [duration, setDuration] = useState(0);
-  const [playbackRate, setPlaybackRate] = useState(1.0);
-  const [loop, setLoop] = useState(false);
-  const [seeking, setSeeking] = useState(false);
+  // const [url, setUrl] = useState<String | null>(null);
+  // const [pip, setPip] = useState(false);
+  // const [playing, setPlaying] = useState(true);
+  // const [controls, setControls] = useState(false);
+  // const [light, setLight] = useState(false);
+  // const [volume, setVolume] = useState(0.8);
+  // const [muted, setMuted] = useState(false);
+  // const [played, setPlayed] = useState(0);
+  // const [loaded, setLoaded] = useState(0);
+  // const [duration, setDuration] = useState(0);
+  // const [playbackRate, setPlaybackRate] = useState(1.0);
+  // const [loop, setLoop] = useState(false);
+  // const [seeking, setSeeking] = useState(false);
 
-  const resetPlaying = () => {
-    setPlayed(0);
-    setLoaded(0);
-    setPip(false);
-  };
+  // const resetPlaying = () => {
+  //   setPlayed(0);
+  //   setLoaded(0);
+  //   setPip(false);
+  // };
 
-  const load = (url: String | null) => {
-    setUrl(url);
-    resetPlaying();
-  };
+  // const load = (url: String | null) => {
+  //   setUrl(url);
+  //   resetPlaying();
+  // };
 
-  const handlePlayPause = () => {
-    setPlaying(!playing);
-  };
+  // // 일시정지
+  // const handlePlayPause = () => {
+  //   setPlaying(!playing);
+  // };
+  // // 재생 중지
+  // const handleStop = () => {
+  //   setUrl(null);
+  //   setPlaying(false);
+  // };
 
-  const handleStop = () => {
-    setUrl(null);
-    setPlaying(false);
-  };
+  // // ToggleControls => 이거 켜면 유튜브 자체 재생 바 켜짐.
+  // const handleToggleControls = () => {
+  //   const playing_url = url;
+  //   setControls(!controls);
+  //   load(playing_url);
+  // };
 
-  const handleToggleControls = () => {
-    const playing_url = url;
-    setControls(!controls);
-    load(playing_url);
-  };
+  // // light하게 표시.
+  // const handleToggleLight = () => {
+  //   setLight(!light);
+  // };
 
-  const handleToggleLight = () => {
-    setLight(!light);
-  };
+  // // 반복 재생
+  // const handleToggleLoop = () => {
+  //   setLoop(loop);
+  // };
 
-  const handleToggleLoop = () => {
-    setLoop(loop);
-  };
+  // // 소리 크기 조정
+  // const handleVolumeChange = e => {
+  //   setVolume(parseFloat(e.target.value));
+  // };
 
-  const handleVolumeChange = e => {
-    setVolume(parseFloat(e.target.value));
-  };
+  // // 음소거
+  // const handleToggleMuted = () => {
+  //   setMuted(!muted);
+  // };
 
-  const handleToggleMuted = () => {
-    setMuted(!muted);
-  };
+  // const handleSetPlaybackRate = e => {
+  //   setPlaybackRate(parseFloat(e.target.value));
+  // };
 
-  const handleSetPlaybackRate = e => {
-    setPlaybackRate(parseFloat(e.target.value));
-  };
+  // const handleOnPlaybackRateChange = speed => {
+  //   setPlaybackRate(parseFloat(speed));
+  // };
 
-  const handleOnPlaybackRateChange = speed => {
-    setPlaybackRate(parseFloat(speed));
-  };
+  // const handleTogglePIP = () => {
+  //   setPip(!pip);
+  // };
 
-  const handleTogglePIP = () => {
-    setPip(!pip);
-  };
+  // const handlePlay = () => {
+  //   console.log('onPlay');
+  //   setPlaying(true);
+  // };
 
-  const handlePlay = () => {
-    console.log('onPlay');
-    setPlaying(true);
-  };
+  // const handleEnablePIP = () => {
+  //   console.log('onEnablePIP');
+  //   setPip(true);
+  // };
 
-  const handleEnablePIP = () => {
-    console.log('onEnablePIP');
-    setPip(true);
-  };
+  // const handleDisablePIP = () => {
+  //   console.log('onDisablePIP');
+  //   setPip(false);
+  // };
 
-  const handleDisablePIP = () => {
-    console.log('onDisablePIP');
-    setPip(false);
-  };
+  // const handlePause = () => {
+  //   console.log('onPause');
+  //   setPlaying(false);
+  // };
 
-  const handlePause = () => {
-    console.log('onPause');
-    setPlaying(false);
-  };
+  // const handleSeekMouseDown = e => {
+  //   setSeeking(true);
+  // };
 
-  const handleSeekMouseDown = e => {
-    setSeeking(true);
-  };
+  // const handleSeekChange = e => {
+  //   setPlayed(parseFloat(e.target.value));
+  // };
 
-  const handleSeekChange = e => {
-    setPlayed(parseFloat(e.target.value));
-  };
+  // const handleSeekMouseUp = e => {
+  //   setSeeking(false);
+  //   player.current.seekTo(parseFloat(e.target.value));
+  // };
 
-  const handleSeekMouseUp = e => {
-    setSeeking(false);
-    player.current.seekTo(parseFloat(e.target.value));
-  };
+  // // const handleProgress = state => {
+  // //   console.log('onProgress', state)
+  // //   // We only want to update time slider if we are not currently seeking
+  // //   if (!this.state.seeking) {
+  // //     this.setState(state)
+  // //   }
+  // // }
 
   // const handleProgress = state => {
-  //   console.log('onProgress', state)
+  //   console.log('onProgress', state);
   //   // We only want to update time slider if we are not currently seeking
-  //   if (!this.state.seeking) {
-  //     this.setState(state)
+  //   if (!seeking) {
+  //     setPlayed(state.played);
   //   }
-  // }
+  // };
 
-  const handleProgress = state => {
-    console.log('onProgress', state);
-    // We only want to update time slider if we are not currently seeking
-    if (!seeking) {
-      setPlayed(state.played);
-    }
-  };
+  // const handleDuration = duration => {
+  //   console.log('onDuration', duration);
+  //   setDuration(duration);
+  // };
 
-  const handleDuration = duration => {
-    console.log('onDuration', duration);
-    setDuration(duration);
-  };
-
-  const handleEnded = () => {
-    console.log('onEnded');
-    setPlaying(loop);
-  };
+  // const handleEnded = () => {
+  //   console.log('onEnded');
+  //   setPlaying(loop);
+  // };
 
   return (
     <div>
       <ReactPlayer
         ref={player}
+        className={'shadow border'}
         url={[
           'https://www.youtube.com/watch?v=FtaW6YMAafk',
           'https://www.youtube.com/watch?v=ww9hZfgC2d8',
         ]}
-        pip={pip}
-        playing={playing}
-        controls={controls}
-        light={light}
-        loop={loop}
-        playbackRate={playbackRate}
-        volume={volume}
-        muted={muted}
-        onReady={() => console.log('onReady')}
-        onStart={() => console.log('onStart')}
-        onPlay={handlePlay}
-        onEnablePIP={handleEnablePIP}
-        onDisablePIP={handleDisablePIP}
-        onPause={handlePause}
-        onBuffer={() => console.log('onBuffer')}
-        onPlaybackRateChange={handleOnPlaybackRateChange}
-        onSeek={e => console.log('onSeek', e)}
-        onEnded={handleEnded}
-        onError={e => console.log('onError', e)}
-        onProgress={handleProgress}
-        onDuration={handleDuration}
+        controls={true}
+        width={900}
+        height={600}
       />
     </div>
   );
--- frontend/src/app/containers/CreateCoverPage/step1.tsx
+++ frontend/src/app/containers/CreateCoverPage/step1.tsx
@@ -3,9 +3,37 @@ import CustomPlayer from './CustomPlayer';
 export type Props = {};
 
 export default function CreateCoverRecordPage(props: Props) {
+  const [isUploaded, setIsUploaded] = useState(false);
   return (
-    <div data-testid="CreateCoverRecordPage">
+    <div
+      data-testid="CreateCoverRecordPage"
+      className="flex flex-col items-center"
+    >
       <CustomPlayer />
+
+      <div data-testid="CreateCoverButtons" className="flex flex-row space-x-4">
+        <button
+          type="button"
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-red-700 disabled:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+        >
+          Cancel
+        </button>
+
+        <button id="upload-button" type="button">
+          upload
+        </button>
+        <button id="recording-button" type="button">
+          record
+        </button>
+
+        <button
+          type="button"
+          disabled={!isUploaded}
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
