ffbf22eb6f7704f3994c9a0a11ed2ccdee29dd4d
GoGiants1 <chooi221@snu.ac.kr>
2021-11-10 21:44:58 +0900

    Remove play button background

--- frontend/src/app/components/Album/index.tsx
+++ frontend/src/app/components/Album/index.tsx
@@ -47,7 +47,7 @@ export default function Album(props: Props) {
   const onPlayClicked = (e: React.FormEvent) => {
     e.preventDefault();
     console.log(Form);
-    player.setTrack(trackexample);
+    player.setIndex(0);
   };
 
   return (
@@ -71,10 +71,10 @@ export default function Album(props: Props) {
       />
       <br></br>
       <form onSubmit={onPlayClicked}>
-        <div className="px-4 py-3 bg-gray-50 text-left sm:px-6">
+        <div className="px-4 py-3 text-left sm:px-6">
           <button
             type="submit"
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Play
           </button>
