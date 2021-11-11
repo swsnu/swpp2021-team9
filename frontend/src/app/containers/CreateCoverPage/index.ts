30ec8099e53d50b3a39d1cf9db7b69dd97364c58
GoGiants1 <chooi221@snu.ac.kr>
2021-11-11 15:33:39 +0900

    Remove unused files

--- frontend/src/app/containers/CreateCoverPage/index.ts
+++ frontend/src/app/containers/CreateCoverPage/index.ts
@@ -1,6 +1,4 @@
 export { default as CreateCoverRecordPage } from './step1';
 export { default as CreateCoverInfoPage } from './step2';
 export { default as CustomPlayer } from './CustomPlayer';
-export { default as AudioVisualizer } from './AudioVisualizer';
-export { default as Canvas } from './Canvas';
 export { default as Waveform } from './Waveform';
