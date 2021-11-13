146c5e7854448dd855f81f905eed480ad9c468b7
zz-zye <htht2808@gmail.com>
2021-11-13 14:11:18 +0900

    fixed resultline console warning

--- frontend/src/app/components/ResultLine/index.test.tsx
+++ frontend/src/app/components/ResultLine/index.test.tsx
@@ -5,14 +5,18 @@ import ResultLine from '.';
 
 test('should render', () => {
   render(
-    <ResultLine
-      title="Mock_Title"
-      singer="Mock_Author"
-      view={100}
-      likes={1000}
-      onLineClicked={() => {}}
-      onPlayClicked={() => {}}
-    />,
+    <table>
+      <tbody>
+        <ResultLine
+          title="Mock_Title"
+          singer="Mock_Author"
+          view={100}
+          likes={1000}
+          onLineClicked={() => {}}
+          onPlayClicked={() => {}}
+        />
+      </tbody>
+    </table>,
   );
   expect(screen.getByTestId('ResultLine')).toBeTruthy();
 });
