536ae07577405a7b76d28d1398007148f2451f03
ccdc-git <titieiti.com@gmail.com>
2021-11-21 23:43:21 +0900

    testing

--- backend/app/band/serializers.py
+++ backend/app/band/serializers.py
@@ -1,8 +1,12 @@
 """ Band serializers
 DRF serializers for band
 """
-from rest_framework import serializers
+from django.utils.translation import override
+from rest_framework import serializers, validators
+
+from user.serializers import UserSerializer
 from .models import (
+    CoverTag,
     Instrument,
     Song,
     Cover,
@@ -29,6 +33,26 @@ class SongSerializer(serializers.ModelSerializer):
 class CoverSerializer(serializers.ModelSerializer):
     """Serializer for cover"""
 
+    user = UserSerializer(many=False, read_only=True)
+    song = SongSerializer(many=False, read_only=True)
+    tags = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+
+    tags_list = serializers.ListField(write_only=True)
+
+    # override
+    # def create(self, validated_data):
+    #     print('create')
+    #     super().create(validated_data)
+
+    # override
+    def update(self, instance, validated_data):
+        instance: Cover = super().update(instance, validated_data)
+        if validated_data.get('tags_list') is not None:
+            tags = CoverTag.objects.filter(name__in=validated_data['tags_list'])
+            instance.tags.set(tags)
+            instance.save()
+        return instance
+
     class Meta:
         model = Cover
         fields = [
@@ -36,14 +60,15 @@ class CoverSerializer(serializers.ModelSerializer):
             "audio",
             "title",
             "category",
-            "descripton",
+            "description",
             "user",
             "instrument",
             "song",
             "tags",
-            "likes",
+            "like_count",
             "views",
             "combination",
+            "tags_list",
         ]
 
 
--- backend/app/bandcruit/settings.py
+++ backend/app/bandcruit/settings.py
@@ -158,6 +158,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
