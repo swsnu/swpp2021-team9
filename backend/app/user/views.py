17dcd2defd0393f897d4dfc51468fd16a482faee
zz-zye <htht2808@gmail.com>
2021-12-01 18:45:25 +0900

    added user views

--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -2,53 +2,64 @@
 views for user
 TODO ("implement")
 """
-from rest_framework import viewsets
+
 from django.http import HttpResponse
 from django.http.request import HttpRequest
 from django.views import View
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from rest_framework import mixins, generics
 from .serializers import UserSerializer
-from .models import CustomUser
+
+User = get_user_model()
+
 
 # pylint: disable=W0613, R0201
 # temporarily disable unused-argument, no-self-use warning
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """ViewSet for user"""
-
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
 
 
 class UserSignup(View):
     """user/signup/"""
 
     def post(self, request: HttpRequest):
-        return HttpResponse(status=501)
+        data = request.data
+        email = data["email"]
+        password = data["password"]
+        User.objects.create_user(email, password)
+        return HttpResponse(status=201)
 
 
 class UserSignin(View):
     """user/signin/"""
 
     def post(self, request: HttpRequest):
-        return HttpResponse(status=501)
+        data = request.data
+        email = data["email"]
+        password = data["password"]
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:  # If user exists
+            login(request, user)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
 
 
 class UserSignout(View):
     """user/signout/"""
 
     def get(self, request: HttpRequest):
-        return HttpResponse(status=501)
+        if request.user.is_authenticated:
+            logout(request)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
 
 
-class UserInfo(View):
-    """user/info/<int:user_id>/"""
+class UserInfo(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    """user/info/`pk:int`/"""
 
-    def get(self, request: HttpRequest, user_id: int):
-        return HttpResponse(status=501)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def put(self, request: HttpRequest, user_id: int):
-        return HttpResponse(status=501)
-
-    def delete(self, request: HttpRequest, user_id: int):
-        return HttpResponse(status=501)
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
