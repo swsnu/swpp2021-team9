8134e4cce01b36406436be321ea5b5bb949fee69
ccdc-git <titieiti.com@gmail.com>
2021-12-09 20:47:13 +0900

    django cors

--- backend/app/bandcruit/settings.py
+++ backend/app/bandcruit/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "band",
     "user",
@@ -78,9 +79,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "bandcruit.urls"
+
+CORS_ORIGIN_WHITELIST = ["http://www.metaband.space", "http://localhost"]
 
 TEMPLATES = [
     {
