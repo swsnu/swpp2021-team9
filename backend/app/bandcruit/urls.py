32d9f1f805006599999c601eaa88680ec2799c3b
ccdc-git <titieiti.com@gmail.com>
2021-12-09 18:09:59 +0900

    open medias

--- backend/app/bandcruit/urls.py
+++ backend/app/bandcruit/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     path("api/", include("band.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
