6e2b6ee879877075abb094e9094877964b8ede11
ccdc-git <titieiti.com@gmail.com>
2021-12-11 16:39:04 +0900

    user filefield

--- backend/app/user/serializers.py
+++ backend/app/user/serializers.py
@@ -2,11 +2,13 @@
 DRF serializers for user
 """
 from rest_framework import serializers
+from common.proxy_file_field import ProxyFileField
 from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for user"""
+    photo = ProxyFileField(allow_empty_file=True)
 
     class Meta:
         model = CustomUser
